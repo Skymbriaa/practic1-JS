84086c4f3c3101d048b94ebdc47b883669937e86
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-08 11:45:58 +0300

    Log primitive types of data

---
+++ index.js
@@ -0,0 +1,9 @@
+// alert('ten');
+// alert(77);
+
+console.log('ten');    //string
+console.log(10.45);    //number
+console.log(10n);      //bigint
+console.log(true);     //boolean
+console.log(undefined);//object
+console.log(null);
